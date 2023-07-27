78442d53e4ec6899b9b89f53ae240b353a065736
Sam <sam.stikeleather@gmail.com>
2023-07-27 18:44:52 -0400

    fixed one to many relationships in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,28 @@ CREATE DATABASE management_db;
 USE management_db;
 
 CREATE TABLE department (
-id INT NOT NULL,
-name VARCHAR(30) NOT NULL,
-PRIMARY KEY (id),
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-id INT NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
 department_id INT NOT NULL,
+FOREIGN KEY (department_id)
+REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-id INT NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT NOT NULL,
+manager_id INT,
+FOREIGN KEY (role_id)
+REFERENCES role(id),
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL
 );
-
