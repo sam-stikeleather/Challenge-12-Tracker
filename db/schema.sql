9b6592e810cca37f7a789e8305c940b18b95e17c
Sam <sam.stikeleather@gmail.com>
2023-07-27 17:57:11 -0400

    created tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,24 @@ CREATE DATABASE management_db;
 
 USE management_db;
 
+CREATE TABLE department (
+id INT NOT NULL,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id),
+);
+
+CREATE TABLE role (
+id INT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+);
+
+CREATE TABLE employee (
+id INT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NOT NULL,
+);
+
