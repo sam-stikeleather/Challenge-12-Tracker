fcbc9a5c05a004c04fa79154cd00bd2814d4477b
Sam <sam.stikeleather@gmail.com>
2023-08-02 17:40:07 -0400

    scrapped server.js/ added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Agent0624Fox',
+  database: 'management_db',
+});
+
