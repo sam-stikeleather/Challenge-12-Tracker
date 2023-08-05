9fd012799a6f1e5c8aa6df0476fc85278bcc0ea2
Sam <sam.stikeleather@gmail.com>
2023-08-05 12:51:03 -0400

    getting back up to date

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ const db = mysql.createConnection({
   database: 'management_db',
 });
 
+// Function to display the main menu and handle user choices
+function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: 'What would you like to do?',
+          choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add department",
+            "Add role",
+            "Add employee",
+            "Update employee role",
+            "Exit",
+          ],
+          name: 'userChoice',
+        },
+      ])
+      .then(handleUserChoice);
+  }
+
+
+  
