81c3e2563f29d885624f53735c7f974a24fe0daa
Sam <sam.stikeleather@gmail.com>
2023-08-05 14:45:17 -0400

    fixed error in seeds.sql that was not allowing the employee table to populate

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,11 +14,12 @@ VALUES ('Sales Lead', 100000, 1),
        ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, NULL),
-       ('Mike', 'Chan', 2, 1),
-       ('Ashley', 'Rodriguez', 3, NULL),
-       ('Kevin', 'Tupik', 4, 3),
-       ('Malia', 'Brown', 5, NULL),
-       ('Sarah', 'Lourd', 6, 5),
-       ('Tom', 'Allen', 7, NULL),
-       ('Tina', 'Lee', 8, 7);
+VALUES
+  ('John', 'Doe', 1, NULL),
+  ('Mike', 'Chan', 2, 1),
+  ('Ashley', 'Rodriguez', 3, NULL),
+  ('Kevin', 'Tupik', 4, 3),
+  ('Malia', 'Brown', 5, NULL),
+  ('Sarah', 'Lourd', 6, 5),
+  ('Tom', 'Allen', 7, NULL),
+
